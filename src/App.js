ddba6758d83eca6990ef9402e5625f0d568ac3a8
Name <myEmail@example.com>
2021-10-12 23:48:25 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Game from './components/Game/Game';
+import './App.scss';
+import { gameReducer, initialState } from './reducer';
+import { GameProvider } from './context';
+
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <GameProvider reducer={gameReducer} initialState={initialState}>
+      <Game />
+    </GameProvider>
   );
 }
 
